8362ba271e3888e9832bb97fee4009d5f8b654f7
snow-man9 <1298763465@qq.com>
2021-03-05 16:27:42 +0800

    Site updated: 2021-03-05 16:27:40

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "C:\Users\HP\Downloads\20210305041545574_easyicon_net_16.ico");
+        $('[rel="icon"]').attr('href', "/funny.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "C:\Users\HP\Downloads\20210305041633211_easyicon_net_16.ico");
+        $('[rel="icon"]').attr('href', "/favicon.ico");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
